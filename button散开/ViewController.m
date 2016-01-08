2138e7cfff601325ad07398cdb64afc7c212bd25
SnowCheng <260732891@qq.com>
2016-01-08 11:14:34 +0800

    添加demo真机调试

--- button散开/ViewController.m
+++ button散开/ViewController.m
@@ -10,7 +10,7 @@
 #import "DisperseBtn.h"
 
 @interface ViewController ()
-
+@property(nonatomic ,weak) UILabel *lab;
 @end
 
 @implementation ViewController
@@ -18,10 +18,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //散开的view
     DisperseBtn *disView = [[DisperseBtn alloc]init];
     disView.frame = CGRectMake(100, 100, 50, 50);
     disView.borderRect = self.view.frame;
-
+    
     
     NSMutableArray *marr = [NSMutableArray array];
     for (int i = 0; i< 8; i++) {
@@ -29,14 +30,32 @@
         NSString *name = [NSString stringWithFormat:@"found_icons_%d",i];
         [btn setBackgroundImage:[UIImage imageNamed:name] forState:UIControlStateNormal];
         [marr addObject:btn];
+        btn.tag = i;
+        [btn addTarget:self action:@selector(buttonTagget:) forControlEvents:UIControlEventTouchUpInside];
     }
     disView.btns = marr;
     [self.view addSubview:disView];
     
+    
+    
+    //调试使用
+    UILabel *lab = [UILabel new];
+    lab.bounds = CGRectMake(0, 0, 300, 200);
+    lab.font = [UIFont systemFontOfSize:30];
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.center = self.view.center;
+    _lab = lab;
+    [self.view addSubview:lab];
+    
+}
+
+-(void)buttonTagget:(UIButton *)sender{
+    NSLog(@"sdf%d",sender.tag);
+    self.lab.text = [NSString stringWithFormat:@"点击了第%d个按钮",sender.tag+1];
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    NSLog(@"touchesBegan");
+    self.lab.text = @"点击屏幕";
 }
 
 @end
